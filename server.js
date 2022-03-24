db8b4688c1bfb815f4a1cb141062af1bf3dd5c4f
Oscar Cortez <oacortez98@gmail.com>
2022-03-23 23:37:30 -0600

    Install GraphQL and express-GraphQL

--- server.js
+++ server.js
@@ -2,6 +2,33 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 const userData = require("./MOCK_DATA.json");
+const graphql = require("graphql");
+const {graphqlHTTP} = require("express-graphql");
+
+
+
+/**
+ * What is a Schema ?
+ Schema is just a combination between mutations and queries
+
+ * What is  Queries ? 
+ Queries is just like in a database it means (e.g:"GET" "getting information")
+
+ * What is a mutation ?
+ When I have a mutation i'm trying to create, update, or delete data
+ */
+
+
+//Creating a GraphQL Schema 
+const RootQuery = "query"
+const Mutation = "mutation"
+
+const schema = new GraphQLSchema({query: RootQuery, mutation: Mutation})
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}))
 
 app.listen(PORT, () => {
   console.log('Your server is running!');
